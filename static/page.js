180ca6f7e34754d8451d64187b716b8d81c28903
9hax <it.fritz@gmx.de>
2019-09-19 23:19:59 +0200

    design work

--- static/page.js
+++ static/page.js
@@ -1,7 +1,6 @@
 document.write(`
 <head>
   <meta charset="utf-8">
-  <title>9hax</title>
   <link rel="stylesheet" href="static/main.css">
   <link rel="stylesheet" href="static/navbar.css">
   <link rel="stylesheet" href="static/fontawesome.css">
@@ -10,26 +9,26 @@ document.write(`
   <meta name="HandheldFriendly" content="true">
 </head>
 
-  <div class="header-nav">
-    <nav class="navbar">
-      <span class="navbar-toggle" id="js-navbar-toggle">
-        <i class="fa fa-bars"></i>
-      </span>
-      <a href="/#" class="logo">9hax</a>
-      <ul class="main-nav" id="js-menu">
-        <li><a href="#" class="nav-links">Home</a></li>
-        <li><a href="#" class="nav-links">Music</a></li>
-        <li><a href="#" class="nav-links">Projects</a></li>
-        <li><a href="#" class="nav-links">School</a></li>
-        <li><a href="#" class="nav-links">Imprint</a></li>
-      </ul>
-      <script type="text/javascript">
-        let mainNav = document.getElementById('js-menu');
-        let navBarToggle = document.getElementById('js-navbar-toggle');
-        navBarToggle.addEventListener('click', function() {
-          mainNav.classList.toggle('active');
-        });
-      </script>
-    </nav>
-  </div>
+<div class="navbar-container">
+  <nav class="navbar">
+    <span class="navbar-toggle" id="js-navbar-toggle">
+      <i class="fa fa-bars"></i>
+    </span>
+    <a href="/#" class="navbar-logo">9hax</a>
+    <ul class="navbar-link-area" id="navbar-collapse-js">
+      <li><a href="#" class="navbar-links">Home</a></li>
+      <li><a href="#" class="navbar-links">Music</a></li>
+      <li><a href="#" class="navbar-links">Projects</a></li>
+      <li><a href="#" class="navbar-links">School</a></li>
+      <li><a href="#" class="navbar-links">Imprint</a></li>
+    </ul>
+    <script type="text/javascript">
+      let mainNav = document.getElementById('navbar-collapse-js');
+      let navBarToggle = document.getElementById('js-navbar-toggle');
+      navBarToggle.addEventListener('click', function() {
+        mainNav.classList.toggle('navbar-active-mobile');
+      });
+    </script>
+  </nav>
+</div>
 `)

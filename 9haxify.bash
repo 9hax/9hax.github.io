17a33119f4269af4e652b5a2233af96299976f1e
9hax <it.fritz@gmx.de>
2020-04-20 22:16:42 +0200

    Update 9haxify.bash

--- 9haxify.bash
+++ 9haxify.bash
@@ -1,15 +1,29 @@
 #!/bin/bash
 
 # This file is used to automatically set up an Ubuntu system with a good looking shell and some cool aliases.
-# It only downloads an installer file, runs it as rot and deletes it afterwards.
-read -p "Do you want to install 9haxify to this system? " -n 1 -r
+
+read -p "Do you want to install 9haxify to this directory? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-mkdir /temp/9haxify
-wget https://9hax.github.io/9haxify/install.bash /temp/9haxify/
-echo "Getting sudo rights to install package and dependencies..."
-sudo bash /temp/9haxify/install.bash
+ echo "Please authenticate."
+ sudo apt-get install -y git aptitude  
+ 
+ echo "Cloning mybashrc Repository..."
+ git clone https://github.com/9hax/mybashrc  
+ 
+ echo "Setting up mybashrc environment in current folder..."
+ mv mybashrc/.* .
+ echo "You are using fancyprompt with mybashrc by 9hax. This is free Software. Github at bit.ly/mbrcgit" > todo
+ 
+ echo "The current todo file is:"
+ source todo
+ 
+ read -p "Installation finished. Do you want to activate your new installation now? (Y) " -n 1 -r
+ if ![[ $REPLY =~ ^[Nn]$ ]]
+ then
+  source .bashrc
+ fi
 
 fi
